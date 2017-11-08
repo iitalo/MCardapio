205ec4d08683c6ae1290cd9445dd053c33984122
Ítalo Silva Costa <italosc91@hotmail.com>
2017-11-07 23:23:20 -0300

    Criaçaõ da MealTask

---
+++ app/src/main/java/br/ufms/facom/mcardapio/MealTask.java
@@ -0,0 +1,14 @@
+package br.ufms.facom.mcardapio;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by italo on 07/11/17.
+ */
+
+public class MealTask extends AsyncTask<String, String, String> {
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
